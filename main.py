be7f96465a365a4d73b2c1dcd70bcd798b65e3d5
chuanshaof <63920704+chuanshaof@users.noreply.github.com>
2021-06-24 20:29:58 +0800

    changing python3 to python in Procfile

--- main.py
+++ main.py
@@ -148,13 +148,13 @@ def main():
     # log all errors
     dp.add_error_handler(error)
 
-    updater.start_polling()
+    # updater.start_polling()
 
-    # # Start the Bot
-    # updater.start_webhook(listen="0.0.0.0",
-    #                       port=int(PORT),
-    #                       url_path=TOKEN)
-    # updater.bot.setWebhook('https://boiling-badlands-67618.herokuapp.com/' + TOKEN)
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://boiling-badlands-67618.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
