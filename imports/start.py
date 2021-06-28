0005469167c23e1ad62111da2249b49bc17f13d2
chuanshaof <63920704+chuanshaof@users.noreply.github.com>
2021-06-28 22:03:25 +0800

    changing python3 to python in Procfile

--- imports/start.py
+++ imports/start.py
@@ -24,27 +24,22 @@ def start(update: Update, context: CallbackContext) -> None:
 
     bot.sendMessage(chat_id=update.message.chat_id,
                     text=f"Hello <b>{update.message.from_user.username}</b> and welcome to "
-                         f"<b>SUTD Productions'</b> Video Project Telegram Bot! 👋\n\n"
+                         f"SUTD Productions' <b>Video Project Telegram Bot!</b> 👋\n\n"
 
                          f"This bot is a one-stop platform for you to share your wildest video ideas, "
                          f"or to join a team with others. Once enough people have opted into your idea, "
-                         f"we will contact you via Telegram to begin. Ideate - find a team - get filming! 🎬\n\n"
+                         f"we will contact you via Telegram to begin. "
+                         f"<b>Ideate - find a team - get filming!</b> 🎬\n\n"
 
                          f"We're excited to have you here! We'll be updating the Bot with project ideas "
                          f"as soon as they're received, so sit tight and get ready to make some videos! 🎥\n\n"
-
-                         f"The available commands on this bot are:\n"
-                         f"/start - Connect with us\n"
-                         f"/subscribe - Subscribe to the telegram bot for notifications\n"
-                         f"/viewprojects - View and join current projects\n\n"
 
                          f"<b>Before you go, remember to:</b>\n"
                          f"📺 Subscribe to our <a href='https://www.youtube.com/user/SUTDProductions'>YouTube</a> channel\n"
                          f"📱 Invite your friends to our <a href='https://t.me/joinchat/SME7jUkjNIcSKc9v'>Telegram</a> community\n"
                          f"📷 Follow us on <a href='https://www.instagram.com/sutdproductions/'>Instagram</a>\n\n"
 
-                         "Feel free to contact us directly at productions@club.sutd.edu.sg for any "
-                         "questions or feedback!",
+                         "Any questions or feedback can be directed to @kong_noah.",
                     parse_mode=ParseMode.HTML,
                     reply_markup=reply_markup)
 
